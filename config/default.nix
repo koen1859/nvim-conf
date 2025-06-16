6725471fcb03a038bc4eb5be8461dd152e017793
koen1859 <koenstevens1859@gmail.com>
2025-06-16 17:31:01 +0200

    modularize

--- config/default.nix
+++ config/default.nix
@@ -1,55 +1,9 @@
 {
   imports = [
+    ./options.nix
     ./keymaps.nix
     ./lsp.nix
     ./ui.nix
     ./plugins.nix
   ];
-  vim = {
-    lsp.enable = true;
-    vimAlias = true;
-    viAlias = true;
-    withNodeJs = true;
-    lineNumberMode = "relNumber";
-    enableLuaLoader = true;
-    preventJunkFiles = true;
-    options = {
-      tabstop = 4;
-      shiftwidth = 2;
-      wrap = false;
-      updatetime = 100;
-      scrolloff = 10;
-      undofile = true;
-      incsearch = true;
-      ignorecase = true;
-      smartcase = true;
-    };
-    globals.maplocalleader = "\\";
-
-    clipboard = {
-      enable = true;
-      registers = "unnamed";
-    };
-
-    spellcheck = {
-      enable = true;
-      languages = ["en"];
-      programmingWordlist.enable = true;
-    };
-
-    autopairs.nvim-autopairs.enable = true;
-    binds = {
-      whichKey.enable = true;
-      cheatsheet.enable = true;
-    };
-    git.enable = true;
-    utility = {
-      oil-nvim.enable = true;
-      surround.enable = true;
-      diffview-nvim.enable = true;
-      images = {
-        image-nvim.enable = false;
-      };
-    };
-  };
 }
