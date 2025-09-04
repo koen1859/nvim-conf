07aff9adbb53d99b12cd7be25c2ba0101e357950
koenstevens <gitlab.slurp563@passmail.net>
2025-09-04 13:58:32 +0200

    toggleterm

--- config/extraplugins.nix
+++ config/extraplugins.nix
@@ -25,5 +25,31 @@
         setup = "require('toggleterm').setup {}";
       };
     };
+    luaConfigRC.toggleterm = ''
+      function send_visual_lines()
+      	-- visual markers only update after leaving visual mode
+      	local esc = vim.api.nvim_replace_termcodes("<esc>", true, false, true)
+      	vim.api.nvim_feedkeys(esc, "x", false)
+
+      	-- get selected text
+      	local start_line = vim.api.nvim_buf_get_mark(0, "<")[1]
+      	local end_line = vim.api.nvim_buf_get_mark(0, ">")[1]
+      	local lines = vim.fn.getline(start_line, end_line)
+
+      	-- send selection with trimmed indent
+      	local toggleterm = require("toggleterm")
+      	local indent = nil
+      	for _, line in ipairs(lines) do
+      		if indent == nil and line:find("[^%s]") ~= nil then
+      			indent = line:find("[^%s]")
+      		end
+      		toggleterm.exec(line:sub(indent or 1), 1)
+      	end
+
+      	toggleterm.exec(string.char(13))
+      end
+
+      vim.keymap.set("n", "<leader>tp", Toggleterm)
+    '';
   };
 }
