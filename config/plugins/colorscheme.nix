4fd0f180f93890de76d055292fb388e83bd62d16
Koen Stevens <mail@koenstevens.nl>
2026-01-24 23:01:46 +0100

    gruvbox

--- config/plugins/colorscheme.nix
+++ config/plugins/colorscheme.nix
@@ -1,29 +1,29 @@
 {pkgs, ...}: {
   vim.extraPlugins = {
-    gruvbox-material = {
-      package = pkgs.vimPlugins.gruvbox-material-nvim;
-      setup = ''
-        require('gruvbox-material').setup({
-          italics = true,
-          contrast = "hard",
-          comments = {
-            italics = true,
-          },
-          background = {
-            transparent = false,
-          },
-          float = {
-            force_background = false,
-            background_color = nil,
-          },
-          signs = {
-            force_background = false,
-            background_color = nil,
-          },
-          customize = nil,
-        })
-      '';
-    };
+    # gruvbox-material = {
+    #   package = pkgs.vimPlugins.gruvbox-material-nvim;
+    #   setup = ''
+    #     require('gruvbox-material').setup({
+    #       italics = true,
+    #       contrast = "hard",
+    #       comments = {
+    #         italics = true,
+    #       },
+    #       background = {
+    #         transparent = true,
+    #       },
+    #       float = {
+    #         force_background = false,
+    #         background_color = nil,
+    #       },
+    #       signs = {
+    #         force_background = false,
+    #         background_color = nil,
+    #       },
+    #       customize = nil,
+    #     })
+    #   '';
+    # };
     gruvbox = {
       package = pkgs.vimPlugins.gruvbox-nvim;
       setup = ''
@@ -46,7 +46,7 @@
           inverse = true,
           contrast = "hard",
           dim_inactive = false,
-          transparent_mode = false,
+          transparent_mode = true,
         })
         vim.cmd("colorscheme gruvbox")
       '';
