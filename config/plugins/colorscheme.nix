22cf23a7e7b1f476b48c730c6cde3a581fd27a66
Koen Stevens <mail@koenstevens.nl>
2026-02-15 23:09:17 +0100

    update

--- config/plugins/colorscheme.nix
+++ config/plugins/colorscheme.nix
@@ -1,56 +1,56 @@
 {pkgs, ...}: {
   vim.extraPlugins = {
-    # gruvbox-material = {
-    #   package = pkgs.vimPlugins.gruvbox-material-nvim;
-    #   setup = ''
-    #     require('gruvbox-material').setup({
-    #       italics = true,
-    #       contrast = "hard",
-    #       comments = {
-    #         italics = true,
-    #       },
-    #       background = {
-    #         transparent = false,
-    #       },
-    #       float = {
-    #         force_background = false,
-    #         background_color = nil,
-    #       },
-    #       signs = {
-    #         force_background = false,
-    #         background_color = nil,
-    #       },
-    #       customize = nil,
-    #     })
-    #   '';
-    # };
-    gruvbox = {
-      package = pkgs.vimPlugins.gruvbox-nvim;
+    gruvbox-material = {
+      package = pkgs.vimPlugins.gruvbox-material-nvim;
       setup = ''
-        require("gruvbox").setup({
-          terminal_colors = true,
-          undercurl = true,
-          underline = true,
-          bold = true,
-          italic = {
-            strings = true,
-            emphasis = true,
-            comments = true,
-            operators = false,
-            folds = true,
-          },
-          strikethrough = true,
-          invert_selection = false,
-          invert_signs = false,
-          invert_tabline = false,
-          inverse = true,
+        require('gruvbox-material').setup({
+          italics = true,
           contrast = "hard",
-          dim_inactive = false,
-          transparent_mode = false,
+          comments = {
+            italics = true,
+          },
+          background = {
+            transparent = false,
+          },
+          float = {
+            force_background = false,
+            background_color = nil,
+          },
+          signs = {
+            force_background = false,
+            background_color = nil,
+          },
+          customize = nil,
         })
-        vim.cmd("colorscheme gruvbox")
       '';
     };
+    # gruvbox = {
+    #   package = pkgs.vimPlugins.gruvbox-nvim;
+    #   setup = ''
+    #     require("gruvbox").setup({
+    #       terminal_colors = true,
+    #       undercurl = true,
+    #       underline = true,
+    #       bold = true,
+    #       italic = {
+    #         strings = true,
+    #         emphasis = true,
+    #         comments = true,
+    #         operators = false,
+    #         folds = true,
+    #       },
+    #       strikethrough = true,
+    #       invert_selection = false,
+    #       invert_signs = false,
+    #       invert_tabline = false,
+    #       inverse = true,
+    #       contrast = "hard",
+    #       dim_inactive = false,
+    #       transparent_mode = false,
+    #     })
+    #     vim.cmd("colorscheme gruvbox")
+    #   '';
+    # };
     # catppuccin = {
     #   package = pkgs.vimPlugins.catppuccin-nvim;
     #   setup = ''
